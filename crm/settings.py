331c329a924efbf1bbd3a66aa115e63b6a7921d4
jefe614 <jeffkimani32@gmail.com>
2024-08-17 13:59:34 +0300

    update

--- crm/settings.py
+++ crm/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-f-2lqznx1ppg%a*k(-u9sg7k^svsd7wdsvk8c57_qrd9xnkr-d'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://react-shop-0h3k.onrender.com/']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
